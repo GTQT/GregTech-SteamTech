70dc043de27a9d9b7c0027e4943453c85945fe7f
喵呜酱睦睦子 <1158677939@qq.com>
2025-04-23 22:57:10 +0800

    Update RecipeMapNoEnergyMultiblockController.java

--- src/main/java/keqing/gtsteam/api/metatileentity/multiblock/RecipeMapNoEnergyMultiblockController.java
+++ src/main/java/keqing/gtsteam/api/metatileentity/multiblock/RecipeMapNoEnergyMultiblockController.java
@@ -1,8 +1,9 @@
 package keqing.gtsteam.api.metatileentity.multiblock;
 
-import com.google.common.collect.Lists;
+import codechicken.lib.render.CCRenderState;
+import codechicken.lib.render.pipeline.IVertexOperation;
+import codechicken.lib.vec.Matrix4;
 import gregtech.api.capability.IMultipleTankHandler;
-import gregtech.api.capability.impl.EnergyContainerList;
 import gregtech.api.capability.impl.FluidTankList;
 import gregtech.api.capability.impl.ItemHandlerList;
 import gregtech.api.items.itemhandlers.GTItemStackHandler;
@@ -12,19 +13,20 @@ import gregtech.api.pattern.PatternMatchContext;
 import gregtech.api.recipes.RecipeMap;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
 public abstract class RecipeMapNoEnergyMultiblockController extends RecipeMapPrimitiveMultiblockController {
-
-    public RecipeMapNoEnergyMultiblockController(ResourceLocation metaTileEntityId, RecipeMap<?> recipeMap) {
-        super(metaTileEntityId, recipeMap);
-        this.resetTileAbilities();
-    }
 
     protected IItemHandlerModifiable inputInventory;
     protected IItemHandlerModifiable outputInventory;
     protected IMultipleTankHandler inputFluidInventory;
     protected IMultipleTankHandler outputFluidInventory;
+    public RecipeMapNoEnergyMultiblockController(ResourceLocation metaTileEntityId, RecipeMap<?> recipeMap) {
+        super(metaTileEntityId, recipeMap);
+        this.resetTileAbilities();
+    }
 
     public IItemHandlerModifiable getInputInventory() {
         return this.inputInventory;
@@ -37,6 +39,7 @@ public abstract class RecipeMapNoEnergyMultiblockController extends RecipeMapPri
     public IMultipleTankHandler getInputFluidInventory() {
         return this.inputFluidInventory;
     }
+
     public IMultipleTankHandler getOutputFluidInventory() {
         return this.outputFluidInventory;
     }
@@ -55,13 +58,20 @@ public abstract class RecipeMapNoEnergyMultiblockController extends RecipeMapPri
 
     private void resetTileAbilities() {
         this.inputInventory = new GTItemStackHandler(this, 0);
-        this.inputFluidInventory = new FluidTankList(true, new IFluidTank[0]);
+        this.inputFluidInventory = new FluidTankList(true);
         this.outputInventory = new GTItemStackHandler(this, 0);
-        this.outputFluidInventory = new FluidTankList(true, new IFluidTank[0]);
+        this.outputFluidInventory = new FluidTankList(true);
     }
 
     protected boolean allowSameFluidFillForOutputs() {
         return true;
     }
 
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void renderMetaTileEntity(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline) {
+        super.renderMetaTileEntity(renderState, translation, pipeline);
+        this.getFrontOverlay().renderOrientedState(renderState, translation, pipeline, getFrontFacing(), true,
+                isStructureFormed());
+    }
 }
